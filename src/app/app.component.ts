16e4b36b374428666533ffce91df142289cbe59b
Antonin BRINES <antonin.brines@outlook.fr>
2023-01-02 14:38:33 +0100

    fix: 🐛 remove unused things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { registerLicense } from '@syncfusion/ej2-base';
 import { environment } from '../environments/environment';
+import { SwUpdate } from '@angular/service-worker';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,11 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent {
   title = 'pwa-client-rdv-14';
-  constructor(public authService: AuthService) {
+  constructor(
+    public authService: AuthService,
+    private swUpdate: SwUpdate,
+    private router: Router
+  ) {
     registerLicense(environment.syncfusionKey);
   }
 
